23841eef8397ecdc14cfa72e86a6456c06155d60
vishalrawat325 <vishalrawat352006@gmail.com>
2025-05-26 21:20:36 +0530

    Update calculator.py

--- Python Basics/calculator.py
+++ Python Basics/calculator.py
@@ -1,1 +1,20 @@
-
+a = 10
+while(a > 0):
+    num1 = int(input("Enter first number : "))
+    opr = input("Enter operator")
+    num2 = int(input("Enter second number : "))
+    if(opr == "+"):
+        print(num1 + num2)
+    elif(opr == "-"):
+        print(num1 - num2)
+    elif(opr == "*"):
+        print(num1 * num2)
+    elif(opr == "/"):
+        print(num1 / num2)
+    elif(opr == "**"):
+        print(num1**num2)
+    elif(opr == "%"):
+        print(num1 % num2)
+    else:
+        print("Invalid Operator")
+    a = a - 1
